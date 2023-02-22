2c24105c79637fb33c036475fa2a156da418f7f3
Lujan Sanchez Cracco <lsanchez@visual-engin.com>
2023-02-22 20:57:26 +0100

    Javascript functions and callstack

---
+++ Javascript Basics/index.js
@@ -0,0 +1,54 @@
+// console.log('START sync code: 1st console.log')
+//---------------------------------
+
+const firstFunction = () => {
+    secondFunction()
+    console.log('FUNCTION sync code: firstFunction')
+}
+
+const secondFunction = () => {
+    thirdFunction()
+    console.log('FUNCTION sync code: secondFunction') 
+}
+
+const thirdFunction = () => {
+    console.log('FUNCTION sync code: thirdFunction')
+}
+
+function oldFunction() {
+    //NOT USED ANYMORE
+}
+//---------------------------------
+
+const doSthFunction = (number1, number2) => {
+    const result = number1 + number2;
+    console.log(result);
+    //DO STH FUNCTIONS DONT HAVE RETURN AND END IN THE CLOSING CURLY BRACE
+}
+
+const sthIsReturned = (number1, number2) => {
+    const result = number1 + number2;
+    return result;
+    //RETURN STH FUNCTIONS ALWAYS FINISH IN THE RETURN NO MATTER WHAT WE HAVE AFTER
+    console.log('this is the eeeend')
+}
+
+const resultDoSthFunction = doSthFunction(1, 1);
+const resultsthIsReturned = sthIsReturned(1, 1);
+
+console.log('resultDoSthFunction result', resultDoSthFunction)
+console.log('resultsthIsReturned result', resultsthIsReturned)
+//---------------------------------
+// fetch("https://gateway.marvel.com/v1/public/characters?apikey=0e10884938787c40366929ce9fde20f4&limit=18").then((response) => {
+//     console.log('Async: This is the backend respose');
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+// setTimeout(() => console.log('Async: setTimeout javascript function')) //ASYNC CALL
+
+// //---------------------------------
+// firstFunction();
+// //---------------------------------
+// console.log('END sync code: 2nd console.log')
+ 
