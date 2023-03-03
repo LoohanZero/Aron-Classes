c895daed2d7a0843d9bf94d0c853b40524466888
Lujan Sanchez Cracco <lsanchez@visual-engin.com>
2023-03-03 20:36:18 +0100

    Adding calcs

--- Javascript Basics/index.js
+++ Javascript Basics/index.js
@@ -8,7 +8,9 @@ const firstFunction = () => {
 
 const secondFunction = () => {
     thirdFunction()
+    setTimeout(() => console.log('FUNCTION ASYNC code: in secondFunction'), 0)
     console.log('FUNCTION sync code: secondFunction') 
+
 }
 
 const thirdFunction = () => {
@@ -20,24 +22,24 @@ function oldFunction() {
 }
 //---------------------------------
 
-const doSthFunction = (number1, number2) => {
-    const result = number1 + number2;
-    console.log(result);
-    //DO STH FUNCTIONS DONT HAVE RETURN AND END IN THE CLOSING CURLY BRACE
-}
+// const doSthFunction = (number1, number2) => {
+//     const result = number1 + number2;
+//     console.log(result);
+//     //DO STH FUNCTIONS DONT HAVE RETURN AND END IN THE CLOSING CURLY BRACE
+// }
 
-const sthIsReturned = (number1, number2) => {
-    const result = number1 + number2;
-    return result;
-    //RETURN STH FUNCTIONS ALWAYS FINISH IN THE RETURN NO MATTER WHAT WE HAVE AFTER
-    console.log('this is the eeeend')
-}
+// const sthIsReturned = (number1, number2) => {
+//     const result = number1 + number2;
+//     return result;
+//     //RETURN STH FUNCTIONS ALWAYS FINISH IN THE RETURN NO MATTER WHAT WE HAVE AFTER
+//     console.log('this is the eeeend')
+// }
 
-const resultDoSthFunction = doSthFunction(1, 1);
-const resultsthIsReturned = sthIsReturned(1, 1);
+// const resultDoSthFunction = doSthFunction(1, 1);
+// const resultsthIsReturned = sthIsReturned(1, 1);
 
-console.log('resultDoSthFunction result', resultDoSthFunction)
-console.log('resultsthIsReturned result', resultsthIsReturned)
+// console.log('resultDoSthFunction result', resultDoSthFunction)
+// console.log('resultsthIsReturned result', resultsthIsReturned)
 //---------------------------------
 // fetch("https://gateway.marvel.com/v1/public/characters?apikey=0e10884938787c40366929ce9fde20f4&limit=18").then((response) => {
 //     console.log('Async: This is the backend respose');
@@ -51,4 +53,11 @@ console.log('resultsthIsReturned result', resultsthIsReturned)
 // firstFunction();
 // //---------------------------------
 // console.log('END sync code: 2nd console.log')
+
+//---------------------------------
+console.log(1 + 1); // 2 - addition
+console.log("1" + 1) //11 - concatenation
+console.log(1 - 1); // 0 - substraction
+console.log(2 * 2); // 4 -multiplication
+console.log(2 / 2) // 1 - division
  
